dbf386609ab91180863285fc0a481d5e79389991
Carlos <carlos.hernandez@grupoconsultel.com>
2021-11-11 14:53:27 -0400

    cambios

---
+++ src/app/models/sesion_response.ts
@@ -0,0 +1,32 @@
+import { CryptoService } from "../shared/services/crypto.service";
+
+export interface SesionResponse {
+    scod?: string;
+    R?: string;
+    keyS?: string;
+    ivS?: string;
+    keyJ?: string;
+    ivJ?: string;
+    M?: string;
+    x?: string;
+}
+
+export class SesionObject {
+
+    private crypto: CryptoService = new CryptoService()
+
+    deserialize(value: SesionResponse): SesionResponse {
+        try {
+            value.M = this.crypto.decryptJsonFixed(value.M)
+            //value.keyS = this.crypto.decryptJsonFixed(value.keyS)
+            value.ivS = this.crypto.decryptJsonFixed(value.ivS)
+            value.keyJ = this.crypto.decryptJsonFixed(value.keyJ)
+            value.ivJ = this.crypto.decryptJsonFixed(value.ivJ)
+            value.scod = this.crypto.decryptJsonFixed(value.scod)
+            
+        } catch (error) {
+            console.log(error)
+        }
+        return value
+    }
+}
