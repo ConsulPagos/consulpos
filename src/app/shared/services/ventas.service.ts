0017be464c169693c5aae15f71c0e366179ee377
arturolinarescp <arturo.linares@grupoconsultel.com>
2021-12-29 10:42:27 -0400

    estados de cuenta

---
+++ src/app/shared/services/ventas.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class VentasService {
+  private scod: String
+
+  constructor(private http: HttpClient) { }
+
+  doSale(data) {
+    var path = `/saveSale`;
+    var headers = new HttpHeaders()
+    headers.set('Content-Type', 'text/plain')
+    headers.set('Accept', 'text/plain');
+    headers.set('Access-Control-Allow-Origin', '*');
+    return this.http.post(`${environment.apiHost}:${environment.puerto_ventas}` + path, data, { headers: headers, responseType: 'text' })
+  }
+
+
+}
