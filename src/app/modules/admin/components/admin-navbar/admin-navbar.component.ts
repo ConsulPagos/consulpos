b08eb2878ec2e46f8f17277d0d38361138604a8f
Carlos <carlos.hernandez@grupoconsultel.com>
2022-01-21 13:18:53 -0400

    session

--- src/app/modules/admin/components/admin-navbar/admin-navbar.component.ts
+++ src/app/modules/admin/components/admin-navbar/admin-navbar.component.ts
@@ -3,6 +3,7 @@ import { FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
 import { LogoutResponse, SesionObject } from 'src/app/models/sesion_response';
 import { CryptoService } from 'src/app/shared/services/crypto.service';
+import { LoaderService } from 'src/app/shared/services/loader.service';
 import { SesionService } from 'src/app/shared/services/sesion.service';
 import { StorageService } from 'src/app/shared/services/storage.service';
 import { ToasterService } from 'src/app/shared/services/toaster.service';
@@ -25,7 +26,7 @@ export class AdminNavbarComponent implements OnInit {
     private sesion: SesionService,
     private crypto: CryptoService,
     private router: Router,
-    private toaster: ToasterService,
+    private loader: LoaderService,
     private route: Router) {
     this.identity = this.storage.getJson(constant.USER).email;
   }
@@ -34,21 +35,18 @@ export class AdminNavbarComponent implements OnInit {
   }
 
   loggout() {
-/*     localStorage.clear()
-    this.route.navigateByUrl('') */
-    
+
     const data = this.crypto.encryptString(JSON.stringify({
       u_id: this.crypto.encryptJson(this.storage.getJson(constant.USER).uid),
       correo: this.crypto.encryptJson(this.storage.getJson(constant.USER).email),
       scod: this.crypto.encryptJson(this.storage.getJson(constant.USER).scod),
-      
     }))
 
+    this.loader.loading()
+
     this.sesion.doLogout(`${this.sesion.getDeviceId()};${data}`).subscribe(res => {
-      console.log(res)
-      console.log(JSON.parse(this.crypto.decryptString(res)))
+      this.loader.stop()
       this.response = new SesionObject(this.crypto).logOutDecrypter(JSON.parse(this.crypto.decryptString(res)))
-      //console.log(this.response)
       localStorage.clear()
       this.route.navigateByUrl('')
     })
