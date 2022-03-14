ed491501ef95485d3bc1e3a8009860641fd6720c
Victor Rivero <victorr2021@gmail.com>
2022-03-14 14:13:50 -0400

    Fixing folder name's typo for deploy

--- scripts/start_service.sh
+++ scripts/start_service.sh
@@ -2,17 +2,19 @@
 
 # give permission to the files inside /secure_docs directory
 
-sudo chmod -R 777 /home/desplieges/front
+sudo chmod -R 777 /home/despliegues/front
 
 # navigate into current working directory
 
-cd /home/desplieges/front
+cd /home/despliegues/front
 
 npm install
 
 run: npm install -g @angular/cli > /dev/null
 
 ng build --prod
+
+rm -rf /var/www/html/*
 
 cp -rf ./dist/* /var/www/html
 
--- scripts/stop_service.sh
+++ scripts/stop_service.sh
@@ -2,8 +2,8 @@
 
 # navigate into current working directory
 
-cd /var/www/html
+# cd /var/www/html
 
 # delete old files
 
-rm -rf *
+# rm -rf *
