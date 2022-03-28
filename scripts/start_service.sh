d413b6253f3e42ab66614769c9f81530c24b9ed9
Victor Rivero <victorr2021@gmail.com>
2022-03-28 11:20:55 -0400

    Testing nvm

--- scripts/start_service.sh
+++ scripts/start_service.sh
@@ -6,7 +6,9 @@ cd /home/despliegues/front
 
 # nvm install 12.20
 
-# npm install -g @angular/cli > /dev/null
+npm install -g node@12.20
+
+npm install -g @angular/cli > /dev/null
 
 ng build --prod
 
