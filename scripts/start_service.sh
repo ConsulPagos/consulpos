d553589b46a8ffe293fe5f3b47dffd69b28a2b78
Victor Rivero <victorr2021@gmail.com>
2022-03-28 10:27:34 -0400

    Testing nvm

--- scripts/start_service.sh
+++ scripts/start_service.sh
@@ -10,6 +10,14 @@ cd /home/despliegues/front
 
 # npm install -g @angular/cli > /dev/null
 
+sudo apt install curl
+
+curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
+
+source ~/.profile 
+
+nvm install 12.20
+
 ng build --prod
 
 # rm -rf /var/www/html/*
