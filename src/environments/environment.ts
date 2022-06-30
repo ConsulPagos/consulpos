1434842a30e2b458dc765373542434e67bfee666
arturolinarescp <arturo.linares@grupoconsultel.com>
2022-06-30 11:01:48 -0400

    V31

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,39 +2,18 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-// export const environment = {
-//   production: false,
-//   apiHost: 'http://192.168.38.10',
-//   puerto_sesion: 9000,
-//   puerto_usuarios: 9001,
-//   puerto_ventas: 9003,
-//   puerto_pagos: 9004,
-//   puerto_clientes: 9005,
-//   puerto_bancario: 9006,
-//   puerto_inventario: 9007,
-//   puerto_archive: 9008,
-//   divider: ":",
-
-//   J_KEY: 'vsPlT8h1qZhMh3gxT0vi4htLMuLvrRRQ',
-//   J_VEC: 'No4cWnM5PQx0zOLB',
-//   S_KEY: "mg5tkziWFsb6trO24DEsRpj00Zj3BV5u",
-//   S_VEC: 'DVpJyVqn2NvUNGDt',
-//   ST_KEY: "NlzvzhuZCcxAwQpbtBjR6JsIo2YJe52Y",
-//   ST_VEC: "3KhhLvac7GAvrKoB",
-// }
-
 export const environment = {
   production: false,
-  apiHost: 'https://administracion.consulpagos.com',
-  puerto_sesion: "",
-  puerto_usuarios: "",
-  puerto_ventas: "",
-  puerto_pagos: "",
-  puerto_bancario: "",
-  puerto_clientes: "",
-  puerto_inventario: "",
-  puerto_archive: "",
-  divider: "",
+  apiHost: 'http://192.168.38.10',
+  puerto_sesion: 9000,
+  puerto_usuarios: 9001,
+  puerto_ventas: 9003,
+  puerto_pagos: 9004,
+  puerto_clientes: 9005,
+  puerto_bancario: 9006,
+  puerto_inventario: 9007,
+  puerto_archive: 9008,
+  divider: ":",
 
   J_KEY: 'vsPlT8h1qZhMh3gxT0vi4htLMuLvrRRQ',
   J_VEC: 'No4cWnM5PQx0zOLB',
@@ -42,7 +21,28 @@ export const environment = {
   S_VEC: 'DVpJyVqn2NvUNGDt',
   ST_KEY: "NlzvzhuZCcxAwQpbtBjR6JsIo2YJe52Y",
   ST_VEC: "3KhhLvac7GAvrKoB",
-};
+}
+
+// export const environment = {
+//   production: false,
+//   apiHost: 'https://administracion.consulpagos.com',
+//   puerto_sesion: "",
+//   puerto_usuarios: "",
+//   puerto_ventas: "",
+//   puerto_pagos: "",
+//   puerto_bancario: "",
+//   puerto_clientes: "",
+//   puerto_inventario: "",
+//   puerto_archive: "",
+//   divider: "",
+
+//   J_KEY: 'vsPlT8h1qZhMh3gxT0vi4htLMuLvrRRQ',
+//   J_VEC: 'No4cWnM5PQx0zOLB',
+//   S_KEY: "mg5tkziWFsb6trO24DEsRpj00Zj3BV5u",
+//   S_VEC: 'DVpJyVqn2NvUNGDt',
+//   ST_KEY: "NlzvzhuZCcxAwQpbtBjR6JsIo2YJe52Y",
+//   ST_VEC: "3KhhLvac7GAvrKoB",
+// };
 
 
 
