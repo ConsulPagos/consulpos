872b6737f22a0c74a143a37117321e0f9c8483fc
arturojose19 <arturojoselinares@hotmail.com>
2022-07-21 15:58:16 -0400

    mejoras

--- src/app/modules/admin/pages/add-pago-manual/add-pago-manual.component.ts
+++ src/app/modules/admin/pages/add-pago-manual/add-pago-manual.component.ts
@@ -1,7 +1,6 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-import { TasaInterface } from 'src/app/models/tasa';
 import { PagosResponse, PagosDecrypter } from 'src/app/models/pagos_response';
 import { SavePagosDecrypter } from 'src/app/models/savepagos_response';
 import { CryptoService } from 'src/app/shared/services/crypto.service';
@@ -9,7 +8,6 @@ import { ModalService } from 'src/app/shared/services/modal.service';
 import { SesionService } from 'src/app/shared/services/sesion.service';
 import { StorageService } from 'src/app/shared/services/storage.service';
 import { ToasterService } from 'src/app/shared/services/toaster.service';
-import { VentasService } from 'src/app/shared/services/ventas.service';
 import { PagosService } from 'src/app/shared/services/pagos.service';
 import { constant } from 'src/app/shared/utils/constant';
 import { LoaderService } from 'src/app/shared/services/loader.service';
@@ -17,6 +15,7 @@ import { DefaultDecrypter } from 'src/app/models/default_response';
 import { BancarioService } from 'src/app/shared/services/bancario.service';
 import { BancoInterface } from 'src/app/models/banco';
 import { ArchiveService } from 'src/app/shared/services/archive.service';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-add-pago-manual',
@@ -41,6 +40,7 @@ export class AddPagoManualComponent implements OnInit {
   totalPago: number = 0;
   bancos: BancoInterface[];
   currentYear = new Date();
+  tipo_tasa: string;
   constructor(
     private crypto: CryptoService,
     private storage: StorageService,
@@ -75,9 +75,13 @@ export class AddPagoManualComponent implements OnInit {
     fecha: new FormControl(''),
   });
 
+  date = new FormGroup({
+    fecha: new FormControl('', [Validators.required]),
+  });
+
   ngOnInit(): void {
     this.getTasas()
-    this.tipoPagos()
+    // this.tipoPagos()
     this.add_pay()
     this.bancos = JSON.parse(this.storage.get(constant.BANCOS)).bancos
   }
@@ -140,35 +144,34 @@ export class AddPagoManualComponent implements OnInit {
 
 
   tipoPagos() {
+    var datePipe = new DatePipe("en-US")
+    const fecha = datePipe.transform(this.date.get('fecha').value, "dd-MM-yyyy")
+    console.log(fecha);
+
+
     const data = this.crypto.encryptString(JSON.stringify({
       u_id: this.crypto.encryptJson(this.storage.getJson(constant.USER).uid),
       correo: this.crypto.encryptJson(this.storage.getJson(constant.USER).email),
       scod: this.crypto.encryptJson(this.storage.getJson(constant.USER).scod),
-      solicitud: this.crypto.encryptJson(this.addPay.solicitud),
-      cod_serial: this.crypto.encryptJson(this.addPay.cod_serial),
-      id: this.crypto.encryptJson(this.addPay.id),
+      solicitud_id: this.crypto.encryptJson(this.addPay.number),
+      t_sol_id: this.crypto.encryptJson(this.addPay.t_sol_id),
+      fecha: this.crypto.encryptJson(fecha),
     }))
     this.loader.loading()
-    this.pago.paymentConstructorManual(`${this.session.getDeviceId()};${data}`).subscribe(res => {
+    this.pago.doPaymentInput(`${this.session.getDeviceId()};${data}`).subscribe(res => {
+      console.log(res)
+      console.log(this.crypto.decryptString(res))
       this.loader.stop()
       const json = JSON.parse(this.crypto.decryptString(res))
-      console.log(this.crypto.decryptString(res))
-      console.log(res)
-      console.log(this.crypto.decryptJson(json.t_pagos))
       this.t_pagos = JSON.parse(this.crypto.decryptJson(json.t_pagos))
       console.log(this.t_pagos)
       this.total = JSON.parse(this.crypto.decryptJson(json.total_dolar))
-      console.log(this.total)
       this.total_Bs = JSON.parse(this.crypto.decryptJson(json.total_Bs))
-      console.log(this.total_Bs)
       this.total_IGTF = JSON.parse(this.crypto.decryptJson(json.total_IGTF))
-      console.log(this.total_IGTF)
       this.total_IVA = JSON.parse(this.crypto.decryptJson(json.total_IVA))
-      console.log(this.total_IVA)
       this.tasa = JSON.parse(this.crypto.decryptJson(json.tasa))
-      console.log(this.tasa)
+      this.tipo_tasa = JSON.parse(this.crypto.decryptJson(json.tipo_tasa))
       this.default = new PagosDecrypter(this.crypto).deserialize(JSON.parse(this.crypto.decryptString(res)))
-      console.log(this.default)
       this.t_pagos = JSON.parse(this.default.t_pagos)
     })
   }
